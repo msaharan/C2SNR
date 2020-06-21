e8cb1a92366bc72c876813acc2a1ee40cf85718a
msaharan <saharan.mohit4@gmail.com>
2020-06-21 22:54:54 +0530

    Add files

---
+++ Pow_spec_test_code/Frequently_used_snippets/Find_kmax_corresponding_to_lmax_for_camb_calculations/kmax_for_lmax.py
@@ -0,0 +1,43 @@
+import numpy as np                                                 
+import cosmolopy.distance as cd
+import camb
+from camb import model, initialpower, get_matter_power_interpolator
+
+###############################################################################
+# Ang dia distance between z = 0 and some redshift
+###############################################################################
+def a_distance(var):
+    return cd.angular_diameter_distance(var, **cosmo)
+#------------------------------------------------------------------------------
+
+###############################################################################
+# Comoving distance between z = 0 and some redshift
+###############################################################################
+def c_distance(var):
+    return cd.comoving_distance(var, **cosmo)
+#------------------------------------------------------------------------------
+
+###############################################################################
+# Set cosmology for Cosmolopy
+###############################################################################
+omegam0 = 0.315
+omegal = 0.685
+c = 3 * 10**8
+H0 = 67.3 * 1000
+h = 0.673
+cosmo = {'omega_M_0': omegam0, 'omega_lambda_0': omegal, 'omega_k_0': 0.0, 'h': 0.673}
+#------------------------------------------------------------------------------
+
+###############################################################################
+# kmax ~ lmax/distance
+###############################################################################
+# redshift = redshift of the source
+# To find k using comoving distance: lmax/c_distance(redshift) 
+# To find k using angular diameter distance: lmax/a_distance(redshift) 
+print("Enter the source redshift (float):")
+redshift = float(input())
+print("Enter lmax (integer):")
+lmax = int(input())
+print("kmax using ang. dia. distance (kmax = lmax/angdiadistance): {}".format(lmax/a_distance(redshift)))
+print("kmax using comoving distance (kmax = lmax/comovingdistance): {}".format(lmax/c_distance(redshift)))
+#------------------------------------------------------------------------------
