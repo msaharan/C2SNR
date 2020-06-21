a85f7dee00ae6b0c811969b7cf34328694a973a0
msaharan <saharan.mohit4@gmail.com>
2020-06-22 03:39:09 +0530

    Update files

--- Pow_spec_test_code/All_tests/Camb_21cm_true_vs_false/linear_angpowspec.py
+++ Pow_spec_test_code/All_tests/Camb_21cm_true_vs_false/linear_angpowspec.py
@@ -1,7 +1,8 @@
+# Note: We can not enable 21 cm options with NonLinear = 2 and NonLinear = 3.
+# See: C2SNR/Pow_spec_test_code/All_tests/Comparison_diff_types_of_camb_non_lin_powspec
+
 import numpy as np
-import math
 import cosmolopy.distance as cd
-import cosmolopy.perturbation as cp
 import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 from scipy import interpolate
@@ -50,13 +51,11 @@ def z_angpowspec_integration_21(ell, redshift):
     constf = z_constantfactor(redshift)
     dist_s = a_distance(redshift)
     return integrate.quad(z_angpowspec_integrand_21, 0.00001, redshift, args = (ell, dist_s, constf))[0]
-
 #------------------------------------------------------------------------------
 
 ###############################################################################
 # Constants
 ###############################################################################
-
 omegam0 = 0.315
 omegal = 0.685
 c = 3 * 10**8
@@ -68,30 +67,29 @@ l_plot_upp_limit = 550
 redshift = 2
 l_max = l_plot_upp_limit
 
-pars21 = model.CAMBparams(NonLinear = 1, WantTransfer = True, H0=67.3, omch2=0.1199, ombh2=0.02205, YHe = 0.24770, Do21cm = True, transfer_21cm_cl = True)
+pars21 = model.CAMBparams(NonLinear = 0, WantTransfer = True, H0=67.3, omch2=0.1199, ombh2=0.02205, YHe = 0.24770, Do21cm = True, transfer_21cm_cl = True)
 pars21.DarkEnergy.set_params(w=-1.13)
 pars21.set_for_lmax(lmax=2500)
 pars21.InitPower.set_params(ns=0.9603, As = 2.196e-09)
 results = camb.get_background(pars21)
 k=np.exp(np.log(10)*np.linspace(-6,1,1000))
-PK21 = get_matter_power_interpolator(pars21, nonlinear=True, kmax = 2)
+PK21 = get_matter_power_interpolator(pars21, nonlinear=False, kmax = 2)
 
-pars = model.CAMBparams(NonLinear = 1, WantTransfer = True, H0=67.3, omch2=0.1199, ombh2=0.02205, YHe = 0.24770)
+pars = model.CAMBparams(NonLinear = 0, WantTransfer = True, H0=67.3, omch2=0.1199, ombh2=0.02205, YHe = 0.24770)
 pars.DarkEnergy.set_params(w=-1.13)
 pars.set_for_lmax(lmax=2500)
 pars.InitPower.set_params(ns=0.9603, As = 2.196e-09)
 results = camb.get_background(pars)
 k=np.exp(np.log(10)*np.linspace(-6,1,1000))
-PK = get_matter_power_interpolator(pars, nonlinear=True, kmax = 2)
-
+PK = get_matter_power_interpolator(pars, nonlinear=False, kmax = 2)
 #------------------------------------------------------------------------------
 
 plt.subplots()
 ###############################################################################
 # To store the data
 ###############################################################################
-z_file_21 = open("./Text_files/angpowspec_integration_over_z_with_21.txt", 'w')
-z_file = open("./Text_files/angpowspec_integration_over_z_without_21.txt", 'w')
+z_file_21 = open("./Text_files/linear_angpowspec_with_21.txt", 'w')
+z_file = open("./Text_files/linear_angpowspec_without_21.txt", 'w')
 #------------------------------------------------------------------------------
 
 ###############################################################################
@@ -104,8 +102,8 @@ for L in tqdm(range(10, int(l_plot_upp_limit))):
 z_file.close()
 z_file_21.close()
 
-L , z_CL = np.loadtxt('./Text_files/angpowspec_integration_over_z_without_21.txt', unpack = True)
-L , z_CL_21 = np.loadtxt('./Text_files/angpowspec_integration_over_z_with_21.txt', unpack = True)
+L , z_CL = np.loadtxt('./Text_files/linear_angpowspec_without_21.txt', unpack = True)
+L , z_CL_21 = np.loadtxt('./Text_files/linear_angpowspec_with_21.txt', unpack = True)
 
 plt.plot(L, z_CL, color='red', label='Without 21', marker = 'o')
 plt.plot(L, z_CL_21, color='blue', label='With 21')
@@ -118,8 +116,6 @@ plt.yscale("log")
 plt.xlim(l_plot_low_limit, l_plot_upp_limit)
 plt.legend()
 #plt.ylim(1E-10,1E-7)
-plt.savefig("./Plots/angpowspec_comparison_with_without_21.pdf")
+plt.savefig("./Plots/linear_angpowspec_comparison_with_without_21.pdf")
 plt.show()
 #------------------------------------------------------------------------------
-
-
