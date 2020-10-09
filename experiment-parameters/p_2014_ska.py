96147925cbc264c972f9b3cb668f71d41432099d
Mohit Saharan <dyskun@yahoo.com>
2020-10-10 00:18:42 +0530

    cleaning the code; can now import cosmology and experiment params; signal plot almost done

---
+++ experiment-parameters/p_2014_ska.py
@@ -0,0 +1,8 @@
+#-- parameters stated below equation in Pourtsidou et al. 2014
+
+T_sys = 50 #-- kelvin 
+t_obs = 2 * 365 * 24 * 3600 #-- seconds
+bandwidth = 40 #-- MHz
+l_max = 19900
+f_cover = 0.06
+
---
+++ lensing-reconstruction-noise/full_plot.py
@@ -0,0 +1,212 @@
+import numpy as np
+import math
+import cosmolopy.distance as cd
+import cosmolopy.perturbation as cp
+import scipy.integrate as integrate
+import matplotlib.pyplot as plt
+from scipy import interpolate
+from tqdm.auto import tqdm
+import camb
+from camb import model, initialpower, get_matter_power_interpolator
+import time
+import /mnt/storage/pdata/Utility/Git/dyskun/C2SNR/experiment-parameters/p_2014_parameters as exp
+import /mnt/storage/pdata/Utility/Git/dyskun/C2SNR/cosmological-parameters/p_2014_cosmology as cos 
+
+########################################################################
+## Job specific parameters
+########################################################################
+
+### angular binning; max and min Fourier mode
+l_ll = 10
+l_ul = 50000
+
+### Discretization of the observed volume along the line of site
+#-- j=0 mode will be made useless due to foreground removal.
+#-- Stated at the end of section 2 in P_2014.
+j_min = 1 
+j_max = 2
+
+### source redshift
+z_s = 2
+
+### plot params
+l_plot_ll = l_ll
+l_plot_ul = 600
+err_stepsize = 200
+
+#------------------------------------------------------------------------
+
+########################################################################
+## Comoving distance between z = 0 and some redshift
+########################################################################
+def distance(var):
+    return cd.comoving_distance(var, **cosmo)
+#------------------------------------------------------------------------
+
+########################################################################
+## hubble ratio H(z)/H0
+########################################################################
+def hubble_ratio(var):
+    return (omegam0 * (1 + var)**3 + omegal)**0.5
+#------------------------------------------------------------------------
+
+########################################################################
+## temperature T(z)
+########################################################################
+
+def Tz(z): #-- in mK
+    return 180 * omegaH1 * h * (1 + z)**2 / hubble_ratio(z)
+
+########################################################################
+## angular power spectrum
+########################################################################
+def C_l_integrand(z, ell):
+    dist = distance(z)
+    return (1 - dist/dist_s)**2 * PK.P(z, ell/dist) / hubble_ratio(z)
+
+def C_l(ell):
+    return constfac * integrate.quad(C_l_integrand, 0.0001, z_s, args = (ell), limit=20000)[0]
+
+def C_lj(ell, j):
+    k_var = ((ell/dist_s)**2 + (2 * 3.14 * j / Len)**2 )**0.5
+    return Tz(z_s)**2 * PK.P(z_s, k_var) / D2xLen
+#------------------------------------------------------------------------
+
+########################################################################
+## constants
+########################################################################
+
+c = 3 * 10**8
+
+### cosmology
+h = cos.h
+H0 = h * 100
+omegam = cos.omegam
+omegab = cos.omegab
+omegac = cos.omegac
+omegamh2 = omegam * h**2
+omegabh2 = omegab * h**2
+omegach2 = omegac * h**2
+YHe = cos.YHe
+As = cos.As
+ns = cos.ns
+
+### experiment parameters
+bandwidth = exp.bandwidth   #-- MHz
+delta_l = exp.delta_l
+f_sky = exp.f_sky
+omegaH1 = exp.omegaH1
+l_max = exp.l_max
+
+### depth (in Mpc) of the observed volume along the line of site
+#-- source: ZZ_2006
+Len = 1.2 * (bandwidth / 0.1) * ((1 + z_s) / 10)**0.5 * (0.15 / omegamh2)**0.5 * h
+
+### the D^2 L factor in equation 4 of P_2014
+D2xLen = distance(z_s)**2 * Len
+
+### lensing reconstruction noise (eq 14 of P_2014)
+
+#### mass moments
+mass_moment_1 = 0.3
+mass_moment_2 = 0.21
+mass_moment_3 = 0.357
+mass_moment_4 = 46.64
+
+#### cshot
+Cshot = Tz(z)**2 * mass_moment_2 / (eta_bar * D2xLen * mass_moment_1**2)
+
+#### thermal/instrumental noise 
+#-- C_l^N in eq. 14 of P_2014
+#-- c_lj^tot = C_lj + C_l^N)
+C_l_N = (2 * 3.14)**3 * T_sys**2 / (bandwidth * t_obs * f_cover**2 * l_max**2)
+
+### number of error points to dsiplay on the SNR plot
+n_err_points = 1 + int((l_plot_max - l_plot_min)/err_stepsize)
+
+### constant factor ouside the integral in equation 20 of P_2014
+constfac = (9/4) * (H0/c)**3 * omegam0**2 
+#-----------------------------------------------------------------------
+
+########################################################################
+## CAMB
+########################################################################
+pars = model.CAMBparams(NonLinear = 1, WantTransfer = True, H0 = 100 * h, omch2 = omegach2, ombh2 = omegabh2, YHe = YHe)
+pars.DarkEnergy.set_params(w = -1)
+pars.set_for_lmax(lmax = l_ul)
+pars.InitPower.set_params(ns = ns, As = As)
+results = camb.get_background(pars)
+k = 10**np.linspace(-5,1,1000)
+PK = get_matter_power_interpolator(pars, nonlinear=True, kmax = np.max(k), k_hunit = True, hubble_units = True)
+#-----------------------------------------------------------------------
+
+
+########################################################################
+## Arrays
+########################################################################
+
+### angular power spectrum 
+#-- signal
+C_l_array = np.zeros(int(2**0.5 * l_ul))
+C_lj_array = np.zeros((int(2**0.5 * l_ul), j_max))
+
+### error bars in the final signal-to-noise ratio plot
+delta_C_L = np.zeros(n_err_points)
+
+### lensing reconstruction noise
+#### noise array
+N_l = np.zeros(n_err_points)
+#### other terms in the noise expression
+N0_sum = np.zeros(n_err_points)
+N1_sum = np.zeros(n_err_points)
+N2_sum = np.zeros(n_err_points)
+N3_sum = np.zeros(n_err_points)
+N4_sum = np.zeros(n_err_points)
+noise_denominator_sum = np.zeros(n_err_points)
+#------------------------------------------------------------------------
+
+########################################################################
+## write data
+########################################################################
+C_l_data = open('./text-files/c_l_data_z_{}_lmax_{}_jmax_{}.txt'.format(z_s, l_ul, j_max - 1), 'w')
+C_lj_data = open('./text-files/c_lj_data_z_{}_lmax_{}_jmax_{}.txt'.format(z_s, l_ul, j_max - 1), 'w')
+N_l_data = open('./text-files/n_l_data_z_{}_lmax_{}_jmax_{}.txt'.format(z_s, l_ul, j_max - 1), 'w')
+delta_C_l_data = open('./text-files/delta_c_l_data_z_{}_lmax_{}_jmax_{}.txt'.format(z_s, l_ul, j_max - 1), 'w')
+
+### C_l
+for L in tqdm(range(l_plot_min, l_plot_max)):
+    plot_curve.write('{}    {}\n'.format(L, C_l(L, z_s)))
+plot_curve.close()
+
+### C_lj
+for L in tqdm(range(l_plot_min, int(l_ul * 2 **0.5))):
+    for j in range(j_min, j_max):
+        C_l_array[L, j] = C_lj(L, j)
+#-----------------------------------------------------------------------
+
+########################################################################
+## write data
+########################################################################
+### canvas
+fig, ax = plt.subplots(figsize=(7,7))
+ax.tick_params(axis='both', which='major', labelsize=fs)
+ax.tick_params(axis='both', which='minor', labelsize=mfs)
+fs = 14
+mfs = 12
+
+### read data
+#-- the initial 'i' denotes input
+iL, iC_l = np.loadtxt('./text-files/c_l_data_z_{}_lmax_{}_jmax_{}.txt'.format(z_s, l_ul, j_max - 1), unpack=True)
+
+### plot
+plt.plot(L, CL, label = '$C_L$', color = 'black')
+plt.xscale('log')
+plt.yscale('log')
+plt.xlabel('$L$', fontsize = fs)
+plt.ylabel('$C_L$', fontsize = fs)
+plt.legend(fontsize = fs)
+plt.xlim(l_plot_min, l_plot_max)
+#plt.ylim(4E-10, 3E-8)
+plt.savefig('./plots/c_l_z_{}_lmax_{}_j_max_{}.pdf'.format(z_s, l_ul, j_max - 1), bbox_inches='tight')
+plt.show()
+
